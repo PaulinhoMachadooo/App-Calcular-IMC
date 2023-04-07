a59b8877b51056317ba7f497acbedb18e9a05238
PaulinhoMachadooo <paulinhomachadoo20@gmail.com>
2023-04-07 20:11:49 -0300

    Api Share

--- src/components/Form/ResultImc/style.js
+++ src/components/Form/ResultImc/style.js
@@ -21,6 +21,27 @@ const styles = StyleSheet.create({
        color:"#ff0043",
        fontWeight:"bold",
    },
+
+   boxSharebutton: {
+       width:"100%",
+       alignItems:"center",
+       marginBottom:10,
+   },
+   shared:{
+       backgroundColor:"#1877f2",
+       borderRadius:50,
+       paddingBottom:5,
+       paddingTop:5,
+       marginBottom:10,
+       alignItems: "center",
+   },
+   sharedText:{
+       color:"#ffffff",
+       fontWeight:"bold",
+       paddingHorizontal: 30,
+      
+   }
+
 });
 
 export default styles
